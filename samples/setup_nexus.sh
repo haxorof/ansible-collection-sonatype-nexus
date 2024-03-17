9e7e27cd37de641b88ec4f2204f4b72bd02f0d67
Bjorn Oscarsson <haxorof@users.noreply.github.com>
2024-03-17 16:47:08 +0100

    Added nexus teardown script

--- samples/setup_nexus.sh
+++ samples/setup_nexus.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # Simple steps to setup Nexus 3 to run samples with.
 # Note! nexus.localdomain must be set in your etc/hosts to where your local test instance will be available.
+NEXUS_HOST=${NEXUS_HOST:-"nexus.localdomain"}
 docker pull sonatype/nexus3
-docker stop nexus
-docker rm nexus --volumes
-docker volume rm nexus_volume
 # Default password would be admin123
 docker run -d -p 8081:8081 --name nexus -v nexus_volume:/nexus-data -e NEXUS_SECURITY_RANDOMPASSWORD=false sonatype/nexus3
-timeout 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://nexus.localdomain:8081/service/rest/v1/status/writable)" != "200" ]]; do sleep 5; done' || false
+timeout 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://$NEXUS_HOST:8081/service/rest/v1/status/writable)" != "200" ]]; do sleep 5; done' || false
---
+++ samples/teardown_nexus.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+docker stop nexus
+docker rm nexus --volumes
+docker volume rm nexus_volume
