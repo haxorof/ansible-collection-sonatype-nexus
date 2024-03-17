e5b18d5736be21ee120fdfb027db87f1166cadf6
Bjorn Oscarsson <haxorof@users.noreply.github.com>
2024-03-17 16:37:11 +0100

    Fix error related to symlink in setup_ansible.sh

--- samples/setup_ansible.sh
+++ samples/setup_ansible.sh
@@ -8,11 +8,13 @@ pip install -U pip
 pip install ansible-core
 pip install -r ./requirements.txt
 
-COLLECTIONS_SYMLINK="$HOME/.ansible/collections/ansible_collections/haxorof/sonatype_nexus"
+COLLECTIONS_HAXOROF_DIR="$HOME/.ansible/collections/ansible_collections/haxorof"
+COLLECTIONS_SYMLINK="$COLLECTIONS_HAXOROF_DIR/sonatype_nexus"
 if [[ -L $COLLECTIONS_SYMLINK ]]; then
     echo "Symbolic link already exists:"
     ls -l $COLLECTIONS_SYMLINK
 else
+    mkdir -p $COLLECTIONS_HAXOROF_DIR
     ln -s $(pwd) $COLLECTIONS_SYMLINK
 fi
 echo "Done!"
